bfc3812091c079e93041516ee8a8ddd2f2515c1d
nightfeaver <nightfeaver99@gmail.com>
2014-10-29 16:25:17 +0000

    Answer found, using snippet from answer done at Sutton

--- week-001/euler/solutions/0003.js
+++ week-001/euler/solutions/0003.js
@@ -22,5 +22,66 @@
 module.exports = function(){
 	var value;
 
+	// declare and init
+	var ceiling;
+	ceiling = 2000001;
+	var primeChecker;
+	primeChecker = 2;
+	var holder;
+	holder = [];
+	var mainCount = 3;
+	var target = 600851475143;
+	var primeFactor=2000000;
+
+// load up the array with zeros
+// for holder, 0 = empty, 1 = non prime, 2 = prime
+
+
+
+	for (i = 0; i < ceiling; i++) {
+		holder[i] = 0;
+	}
+
+// get rid of evens
+	for (i = primeChecker; i*primeChecker < ceiling; i++) {
+		holder[i * primeChecker] = "Not Prime"
+	}
+
+
+// Loop through 2 million numbers to find primes
+
+	while (mainCount < ceiling) {
+
+		// is the next number a prime
+		if (holder[primeChecker + 1] === 0)
+		// yes
+		// store prime
+		{
+			var nextPrime= primeChecker+1;
+			holder[nextPrime] = nextPrime;
+			// remove multiples
+
+			for (i = 2; i*(nextPrime) < ceiling; i++) {
+				holder[i*nextPrime] = "Not Prime";
+			}
+			// update counters
+			mainCount++;
+			primeChecker = primeChecker + 1;
+		}
+		else {
+			// updae counters
+			mainCount++;
+			primeChecker = primeChecker + 1;
+		}
+		//for (i = 2; i < ceiling; i++) if (holder[i] === 2) {
+		//    console.log(i)
+		//}
+	}
+
+	while(primeFactor>1) {
+		if(target % holder[primeFactor] === 0) {value=primeFactor;break
+		} else {primeFactor--}
+	}
+
 	return value;
 };
